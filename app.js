f7551df811beaf3a31bed62cad7fb2fb170a0d45
FefeMT <felipemtanoira@gmail.com>
2022-07-13 22:03:36 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const publicPath = path.resolve(__dirname,"./public");
+
+app.use(express.static(publicPath));
+
+const start = () => {
+    console.log("Arranca")
+};
+
+app.listen(port,start);
+
+app.get("/", function(req,res){
+    res.sendFile(path.resolve(__dirname,"views","home.html"))
+});
+
+app.get("/register", function(req,res){
+    res.sendFile(path.resolve(__dirname,"views","register.html"))
+});
+
+app.get("/login", function(req,res){
+    res.sendFile(path.resolve(__dirname,"views","login.html"))
+});
+
+// desktop 1280px
